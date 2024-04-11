e65e05934fcad2924ca975e2896d8c24e6cf4b0f
Paulo Santos <paulo.jorge.ngs@gmail.com>
2024-04-11 22:30:11 +0100

    Remove unused import and add placeholder text

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,77 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <header className="App-header">
+        <h3>Project Name</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+          lacus risus, dapibus nec lacus non, mattis rutrum ligula. Aenean
+          efficitur nec quam a euismod. Fusce ut porta tortor. Mauris nec
+          tincidunt neque. Etiam turpis diam, accumsan sed ultricies nec,
+          dignissim sed lectus. Donec sed vulputate quam. Donec faucibus
+          convallis mi, et interdum metus pharetra ac. Sed enim enim, euismod
+          non nunc vel, pharetra porttitor justo. Praesent eleifend tortor
+          risus, a accumsan urna placerat nec. Cras in malesuada dui, at pretium
+          massa. Sed in ante consectetur, venenatis justo ac, euismod orci.
+          Aliquam ac pulvinar augue, bibendum malesuada ex. Fusce pretium nisi
+          ac sapien viverra, non luctus velit laoreet. Fusce neque tortor,
+          posuere id lectus id, malesuada varius lectus.
+        </p>
+      </header>
+      <header className="App-header">
+        <h3>Project Name</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+          lacus risus, dapibus nec lacus non, mattis rutrum ligula. Aenean
+          efficitur nec quam a euismod. Fusce ut porta tortor. Mauris nec
+          tincidunt neque. Etiam turpis diam, accumsan sed ultricies nec,
+          dignissim sed lectus. Donec sed vulputate quam. Donec faucibus
+          convallis mi, et interdum metus pharetra ac. Sed enim enim, euismod
+          non nunc vel, pharetra porttitor justo. Praesent eleifend tortor
+          risus, a accumsan urna placerat nec. Cras in malesuada dui, at pretium
+          massa. Sed in ante consectetur, venenatis justo ac, euismod orci.
+          Aliquam ac pulvinar augue, bibendum malesuada ex. Fusce pretium nisi
+          ac sapien viverra, non luctus velit laoreet. Fusce neque tortor,
+          posuere id lectus id, malesuada varius lectus.
+        </p>
+      </header>
+      <header className="App-header">
+        <h3>Project Name</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+          lacus risus, dapibus nec lacus non, mattis rutrum ligula. Aenean
+          efficitur nec quam a euismod. Fusce ut porta tortor. Mauris nec
+          tincidunt neque. Etiam turpis diam, accumsan sed ultricies nec,
+          dignissim sed lectus. Donec sed vulputate quam. Donec faucibus
+          convallis mi, et interdum metus pharetra ac. Sed enim enim, euismod
+          non nunc vel, pharetra porttitor justo. Praesent eleifend tortor
+          risus, a accumsan urna placerat nec. Cras in malesuada dui, at pretium
+          massa. Sed in ante consectetur, venenatis justo ac, euismod orci.
+          Aliquam ac pulvinar augue, bibendum malesuada ex. Fusce pretium nisi
+          ac sapien viverra, non luctus velit laoreet. Fusce neque tortor,
+          posuere id lectus id, malesuada varius lectus.
+        </p>
+      </header>
+      <header className="App-header">
+        <h3>Project Name</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+          lacus risus, dapibus nec lacus non, mattis rutrum ligula. Aenean
+          efficitur nec quam a euismod. Fusce ut porta tortor. Mauris nec
+          tincidunt neque. Etiam turpis diam, accumsan sed ultricies nec,
+          dignissim sed lectus. Donec sed vulputate quam. Donec faucibus
+          convallis mi, et interdum metus pharetra ac. Sed enim enim, euismod
+          non nunc vel, pharetra porttitor justo. Praesent eleifend tortor
+          risus, a accumsan urna placerat nec. Cras in malesuada dui, at pretium
+          massa. Sed in ante consectetur, venenatis justo ac, euismod orci.
+          Aliquam ac pulvinar augue, bibendum malesuada ex. Fusce pretium nisi
+          ac sapien viverra, non luctus velit laoreet. Fusce neque tortor,
+          posuere id lectus id, malesuada varius lectus.
+        </p>
+      </header>
       <header className="App-header">
         <h3>Project Name</h3>
         <p>
