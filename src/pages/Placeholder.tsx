6b8cd57c1aa1511b10b700add4d7fc018321a11b
Paulo Santos <paulo.jorge.ngs@gmail.com>
2024-04-11 22:48:26 +0100

    Create components directory and pages directory

---
+++ src/pages/Placeholder.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Placeholder = () => {
+  return (
+    <div>
+      <h1>Placholder</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+        lacus risus, dapibus nec lacus non, mattis rutrum ligula. Aenean
+        efficitur nec quam a euismod. Fusce ut porta tortor. Mauris nec
+        tincidunt neque. Etiam turpis diam, accumsan sed ultricies nec,
+        dignissim sed lectus. Donec sed vulputate quam. Donec faucibus convallis
+        mi, et interdum metus pharetra ac. Sed enim enim, euismod non nunc vel,
+        pharetra porttitor justo. Praesent eleifend tortor risus, a accumsan
+        urna placerat nec. Cras in malesuada dui, at pretium massa. Sed in ante
+        consectetur, venenatis justo ac, euismod orci. Aliquam ac pulvinar
+        augue, bibendum malesuada ex. Fusce pretium nisi ac sapien viverra, non
+        luctus velit laoreet. Fusce neque tortor, posuere id lectus id,
+        malesuada varius lectus.
+      </p>
+    </div>
+  );
+};
+
+export default Placeholder;
